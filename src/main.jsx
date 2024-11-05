6f90267b829a7bcf9375566f952111a8f913cff6
Kim Ji Eun <{>
2024-11-05 21:27:12 +0900

    애플리케이션에 store 주입

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { Provider } from 'react-redux';
 import store from './redux/config/configStore.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store} />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
